b0bc5f1b8e72b7f86ec3cf6ef8f1786ece041458
madhawan17 <aryamadhawan@gmail.ingit config --global user.email aryamadhawan@gmail.com>
2025-07-20 12:05:21 +0530

    new

--- script.js
+++ script.js
@@ -80,17 +80,17 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 // SLICE
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
-console.log(arr.slice(-2, ));
-console.log(arr.slice(-1, ));
-console.log(arr.slice(1,-2 ));
-console.log(arr.slice( ));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
 console.log([...arr]);
 
 //SPLICE
 // console.log(arr.splice(2));
 arr.splice(-1);
 console.log(arr);
-arr.splice(1,2);
+arr.splice(1, 2);
 console.log(arr);
 
 // REVERSE
@@ -105,4 +105,10 @@ console.log(letters);
 console.log([...arr, ...arr2]);
 
 //JOIN
+console.log([letters.join('-')]);
+console.log([letters.join('-')]);
+console.log([letters.join('-')]);
+console.log([letters.join('-')]);
+console.log([letters.join('-')]);
+console.log([letters.join('-')]);
 console.log([letters.join('-')]);
